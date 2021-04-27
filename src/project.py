4c83520d55b962954ddafd9e9b55d3598a92fa8c
GlaDiatoRs_notebook <valdiszz53@gmail.com>
2021-04-27 22:13:42 +0300

    some changes

--- src/project.py
+++ src/project.py
@@ -5,7 +5,19 @@ from src.config import (
     ROOT_PATH, SRC_PATH, SCSS_PATH, COMPONENTS_PATH,
     Paths, Path
 )
-# from src.file import File
+from src.file import File
+# from src.component import Component
+
+# Select components view (as dict or as object)
+# 'component": {
+#   'Test': {
+#       'Test.vue': None,
+#       'TestTest.vue': None
+#   },
+#   'blank': {
+#       'About.vue': None
+#   }
+# }
 
 
 class Project:
@@ -18,10 +30,51 @@ class Project:
 
     def __init__(self):
         self._check_project_dir()
-        # self.vue_files = [
-        #     item
-        #     for item in self._get_files(self._PATH.components)
-        # ]
+        self.vue_files = [
+            File(path, '', self._PATH.components)
+            for path in self._get_files(self._PATH.components)
+        ]
+        print(self.vue_files)
+
+    def _check_project_dir(self):
+        """
+            Check current dir
+
+            :return: sys.exit if src or package-lock.json not in dir
+        """
+        list_check = ['src', 'package-lock.json']
+        for item in self.root_dirs:
+            if item in list_check:
+                list_check.remove(item)
+        if list_check:
+            raise Exception("It's not a project")
+
+    def _get_files(self, path: Path):
+        """
+        :param path: path to directory with files
+        :return: all files from path
+        """
+        files = []
+        for item in path.iterdir():
+            if item.is_file():
+                files.append(item)
+            elif item.is_dir():
+                files.extend(self._get_files(path / item))
+        return files
+
+    def _print_files(self, path: Path, rel_path: Path):
+        # TODO: Create function print tree for print from dict[str: list]
+        parts = path.relative_to(rel_path).parts
+        indent = '\t' * (len(parts) - 1)
+        sep = '|-- ' if len(parts) else ''
+
+        print(indent + sep + path.name)
+        for item in path.iterdir():
+            if item.is_file():
+                print((indent + '\t') + sep + item.name)
+                pass
+            elif item.is_dir():
+                self._print_files(path / item, rel_path)
 
     @property
     def root_dirs(self):
@@ -39,49 +92,13 @@ class Project:
     def component_dirs(self):
         return listdir(self._PATH.components)
 
-    def _check_project_dir(self):
-        """
-            Check current dir
-
-            :return: sys.exit if src or package-lock.json not in dir
-        """
-        list_check = ['src', 'package-lock.json']
-        for item in self.root_dirs:
-            if item in list_check:
-                list_check.remove(item)
-        if list_check:
-            raise Exception("It's not a project")
-
-    def _get_files(self, path: Path):
-        files = []
-        for item in path.iterdir():
-            if item.is_file():
-                files.append(item)
-            elif item.is_dir():
-                files.extend([self._get_files(path / item)])
-        return files
-
-    def _print_files(self, path: Path, rel_path: Path):
-        # TODO: Create function print tree for print from dict[str: list]
-        parts = path.relative_to(rel_path).parts
-        indent = '\t' * (len(parts) - 1)
-        sep = '|-- ' if len(parts) else ''
-
-        print(indent + sep + path.name)
-        for item in path.iterdir():
-            if item.is_file():
-                print((indent + '\t') + sep + item.name)
-                pass
-            elif item.is_dir():
-                self._print_files(path / item, rel_path)
-
     def print_table(self):
         table_data = [
-            ['Components', 'styles']
+            ['Parent', 'Components', 'Styles']
         ]
         table_data.extend(
-            [component[0], component[1]]
-            for component in zip(self._get_files(self._PATH.components), self._get_files(self._PATH.scss))
+            [component.relative_path.parent, component.name, '']
+            for component in self.vue_files
         )
         table = AsciiTable(table_data)
         print(table.table)
